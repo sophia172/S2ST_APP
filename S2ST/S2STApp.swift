98961f073bf23378a99b5c5490d8a9b7e238e5e2
sophia17_2 <sophia17_2@hotmail.com>
2024-04-24 21:50:11 +0100

    Initial Commit

---
+++ S2ST/S2STApp.swift
@@ -0,0 +1,17 @@
+//
+//  S2STApp.swift
+//  S2ST
+//
+//  Created by Ying Liu on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct S2STApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
